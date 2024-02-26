45624415e4c9787de05fd80071a0d22b43045f56
Suchait Gaurav <>
2024-02-26 19:54:59 +0530

    updating helm files with configs.

--- src/test/java/com/user/service/UserEServiceApplicationTests.java
+++ src/test/java/com/user/service/UserEServiceApplicationTests.java
@@ -3,10 +3,10 @@ package com.user.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class UserEServiceApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
